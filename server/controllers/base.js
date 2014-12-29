1a8346b730c54b2d00ba32c5b9bccdc90ce19801
Parker <parkerproject@gmail.com>
2014-12-29 16:00:22 -0500

    updates

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -4,7 +4,7 @@ module.exports = {
         handler: function(request, reply) {
             // Render the view with the custom greeting
             reply.view('index', {
-                title: 'Daily deals, coupons - Dealsbox'
+                title: 'Find quality deals every day - Dealsbox'
             });
         },
         app: {
