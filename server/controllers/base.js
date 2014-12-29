3376b3ea5fc21f1056e5f98f92ba788bb151259d
Parker <parkerproject@gmail.com>
2014-12-29 15:25:08 -0500

    updates to pages

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -1,10 +1,10 @@
 // This is the base controller. Used for base routes, such as the default index/root path, 404 error pages, and others.
 module.exports = {
     index: {
-        handler: function(request, reply){
-          // Render the view with the custom greeting
+        handler: function(request, reply) {
+            // Render the view with the custom greeting
             reply.view('index', {
-                title: 'Daily deals, coupons - BOXEDSALES'
+                title: 'Daily deals, coupons - Dealsbox'
             });
         },
         app: {
@@ -12,7 +12,7 @@ module.exports = {
         }
     },
     about: {
-        handler: function(request, reply){
+        handler: function(request, reply) {
             reply.view('about', {
                 title: 'This is the example about page'
             });
@@ -22,7 +22,7 @@ module.exports = {
         }
     },
     missing: {
-        handler: function(request, reply){
+        handler: function(request, reply) {
             reply.view('404', {
                 title: 'You found a missing page, but won the 404 error!'
             }).code(404);
--- server/controllers/email.js
+++ server/controllers/email.js
@@ -1,34 +1,24 @@
 require('dotenv').load();
-var Kaiseki = require('kaiseki');
-var kaiseki = new Kaiseki(process.env.PARSE_APP_ID, process.env.PARSE_REST_API_KEY);
-var className = 'emails';
+var collections = ['early_access'];
+var db = require("mongojs").connect(process.env.DEALSBOX_MONGODB_URL, collections);
 
 module.exports = {
     storeEmail: {
         handler: function(request, reply) {
-
-            var emails = [{
-                email: request.params.email
-            }]
-
-            var params = {
-                where: {
-                    email: request.params.email
-                }
-            };
-
-            kaiseki.getObjects(className, params, function(err, res, body, success) {
-                if (body.length === 0) {
-                    kaiseki.createObjects(className, emails, function(err, res, body, success) {
-                        if (success) {
-                            reply('Thanks for signing up. We can\'t wait to launch this.');
-                        } else {
-                            reply('oops! looks like the server failed. Try again');
-                        }
-
-                    });
+            var email = request.params.email;
+            db.early_access.update({
+                email: email
+            }, {
+                email: email
+            }, {
+                upsert: true
+            }, function(err, success) {
+                if (err) console.log(err);
+                if (success) {
+                    reply('Thanks for signing up for early access, We shall be in touch soon.');
                 } else {
-                    reply('You have already signup!');
+                    reply('<span class="error">oops! looks like the server failed. Try again</span>');
+
                 }
             });
 
