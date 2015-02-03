a70cd5806793c00fdc915296f911a85c89ffcdb7
parkerproject <parkerproject@gmail.com>
2015-02-03 16:07:55 +0000

    added replies

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -9,78 +9,79 @@ var _ = require('underscore');
 var mandrill = require('node-mandrill')(process.env.MANDRILL);
 
 function saveEmail(email, reply) {
-    db.early_access.save({
-        email: email
-    }, function(err, success) {
-        console.log(success);
-        if (err) reply('<span class="error">oops! looks like the server failed. Try again</span>');
-        if (success) reply(1);
-    });
+  db.early_access.save({
+    email: email
+  }, function(err, success) {
+    console.log(success);
+    if (err) reply('<span class="error">oops! looks like the server failed. Try again</span>');
+    if (success) reply(1);
+  });
 
 }
 
 function sendEmails(email, subject, content) {
-    mandrill('/messages/send', {
-        message: {
-            to: [{
-                email: email
-            }],
-            from_email: 'no-reply@dealsbox.co',
-            subject: subject,
-            html: content
-        }
-    }, function(error, response) {
-        //uh oh, there was an error
-        if (error) console.log(JSON.stringify(error));
+  mandrill('/messages/send', {
+    message: {
+      to: [{
+        email: email
+      }],
+      from_email: 'no-reply@dealsbox.co',
+      subject: subject,
+      html: content
+    }
+  }, function(error, response) {
+    //uh oh, there was an error
+    if (error) console.log(JSON.stringify(error));
 
-        //everything's good, lets see what mandrill said
-        else console.log(response);
-    });
+    //everything's good, lets see what mandrill said
+    else console.log(response);
+  });
 }
 
 
 module.exports = {
-    storeEmail: {
-        handler: function(request, reply) {
-            var email = request.params.email;
-            db.early_access.findOne({
-                email: email
-            }, function(err, result) {
-                if (err) console.log(err);
-                if (result) {
-                    reply('You have already submitted your email.');
-                } else {
-                    saveEmail(email, reply);
-                }
-            });
-
-
-        },
-        app: {
-            name: 'storeEmail'
+  storeEmail: {
+    handler: function(request, reply) {
+      var email = request.params.email;
+      db.early_access.findOne({
+        email: email
+      }, function(err, result) {
+        if (err) console.log(err);
+        if (result) {
+          reply('You have already submitted your email.');
+        } else {
+          saveEmail(email, reply);
         }
+      });
+
+
     },
-
-    welcomeEmail: {
-        handler: function(request, reply) {
-            var email = request.params.email;
-            var name = request.params.name;
-
-            swig.renderFile(__base + 'views/welcome.html', {
-                    name: name
-                },
-                function(err, content) {
-                    if (err) {
-                        throw err;
-                    }
-                    sendEmails(email, subject, content);
-                });
-
-
-        },
-        app: {
-            name: 'welcomeEmail'
-        }
+    app: {
+      name: 'storeEmail'
     }
+  },
+
+  welcomeEmail: {
+    handler: function(request, reply) {
+      var email = request.params.email;
+      var name = request.params.name;
+
+      swig.renderFile(__base + 'views/welcome.html', {
+          name: name
+        },
+        function(err, content) {
+          if (err) {
+            throw err;
+          }
+          sendEmails(email, subject, content);
+          reply('Email sent');
+        });
+
+
+    },
+    app: {
+      name: 'welcomeEmail'
+    }
+  }
 
 };
