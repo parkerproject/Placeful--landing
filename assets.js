5bac34204d68455609b456800aa4dcc9df4b97b9
Parker <parkerproject@gmail.com>
2015-02-12 22:26:12 -0500

    updates

--- assets.js
+++ assets.js
@@ -1,11 +1,11 @@
 // assets to be used by the 'hapi-assets' module based on process.env.NODE_ENV
 module.exports = {
-  development: {
-    js: ['js/jquery.min.js','js/sweet-alert.js', 'js/scripts.js'],
-    css: ['css/animate.css', 'css/normalize.css', 'css/skeleton.css','css/sweet-alert.css', 'css/app.css']
-  },
-  production: {
-    js: ['js/scripts.min.js'],
-    css: ['css/app.min.css']
-  }
+    development: {
+        js: ['js/jquery.min.js', 'js/sweet-alert.js', 'js/scripts.js'],
+        css: ['css/animate.css', 'css/normalize.css', 'css/skeleton.css', 'css/sweet-alert.css', 'css/app.css']
+    },
+    production: {
+        js: ['js/scripts.min.js'],
+        css: ['css/app.min.css']
+    }
 };
