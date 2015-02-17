c1eb3da5f28666951c45ecf8ca56b5f1a945c2da
Parker <parkerproject@gmail.com>
2015-02-16 19:13:19 -0500

    updates

--- server/controllers/email.js
+++ server/controllers/email.js
@@ -1,5 +1,5 @@
 require('dotenv').load();
-var collections = ['early_access'];
+var collections = ['early_access', 'bloggers'];
 var db = require("mongojs").connect(process.env.DEALSBOX_MONGODB_URL, collections);
 var swig = require('swig');
 var rp = require('request-promise');
@@ -9,89 +9,125 @@ var _ = require('underscore');
 var mandrill = require('node-mandrill')(process.env.MANDRILL);
 
 function saveEmail(data, reply) {
-  db.early_access.save({
-    email: data.email,
-		referral: data.hash
-  }, function(err, success) {
-    console.log(success);
-    if (err) reply('<span class="error">oops! looks like the server failed. Try again</span>');
-    if (success) reply(1);
-  });
+    db.early_access.save({
+        email: data.email,
+        referral: data.hash
+    }, function(err, success) {
+        console.log(success);
+        if (err) reply('<span class="error">oops! looks like the server failed. Try again</span>');
+        if (success) reply(1);
+    });
 
 }
 
 function sendEmails(email, subject, content) {
-  mandrill('/messages/send', {
-    message: {
-      to: [{
-        email: email
-      }],
-      from_email: 'no-reply@dealsbox.co',
-			from_name: 'Dealsbox Team',
-      subject: subject,
-      html: content
-    }
-  }, function(error, response) {
-    //uh oh, there was an error
-    if (error) console.log(JSON.stringify(error));
+    mandrill('/messages/send', {
+        message: {
+            to: [{
+                email: email
+            }],
+            from_email: 'parker@dealsbox.co',
+            from_name: 'Parker from DEALSBOX',
+            subject: subject,
+            html: content
+        }
+    }, function(error, response) {
+        //uh oh, there was an error
+        if (error) console.log(JSON.stringify(error));
 
-    //everything's good, lets see what mandrill said
-    else console.log(response);
-  });
+        //everything's good, lets see what mandrill said
+        else console.log(response);
+    });
 }
 
 
 
 module.exports = {
-  storeEmail: {
-    handler: function(request, reply) {
-      var user = request.params.email;
-			var email = user.split('/')[0];
-			var hash = user.split('/')[1];
+    storeEmail: {
+        handler: function(request, reply) {
+            var user = request.params.email;
+            var email = user.split('/')[0];
+            var hash = user.split('/')[1];
 
-			
-			
-      db.early_access.findOne({
-        email: email
-      }, function(err, result) {
-        if (err) console.log(err);
-        if (result) {
-          reply('You have already submitted your email.');
-        } else {
-          saveEmail({email: email, hash: hash}, reply);
-        }
-      });
+            db.early_access.findOne({
+                email: email
+            }, function(err, result) {
+                if (err) console.log(err);
+                if (result) {
+                    reply('You have already submitted your email.');
+                } else {
+                    saveEmail({
+                        email: email,
+                        hash: hash
+                    }, reply);
+                }
+            });
 
 
-    },
-    app: {
-      name: 'storeEmail'
-    }
-  },
-
-  welcomeEmail: {
-    handler: function(request, reply) {
-      var user = request.params.user;
-			var email = user.split('/')[0];
-      var name = user.split('/')[1];
-			var subject = 'Welcome to Dealsbox';
-
-      swig.renderFile(__base + 'server/views/welcome_email.html', {
-          name: name
         },
-        function(err, content) {
-          if (err) {
-            throw err;
-          }
-          sendEmails(email, subject, content);
-          reply('Email sent');
-        });
-
-
+        app: {
+            name: 'storeEmail'
+        }
     },
-    app: {
-      name: 'welcomeEmail'
+
+    welcomeEmail: {
+        handler: function(request, reply) {
+            var user = request.params.user;
+            var email = user.split('/')[0];
+            var name = user.split('/')[1];
+            var subject = 'Welcome to Dealsbox';
+
+            swig.renderFile(__base + 'server/views/welcome_email.html', {
+                    name: name
+                },
+                function(err, content) {
+                    if (err) {
+                        throw err;
+                    }
+                    sendEmails(email, subject, content);
+                    reply('Email sent');
+                });
+
+
+        },
+        app: {
+            name: 'welcomeEmail'
+        }
+    },
+
+    guestEmail: {
+        handler: function(request, reply) {
+
+            db.bloggers.find(function(err, docs) {
+                var subject = 'Guest post request';
+
+                sendEmails(email, subject, 'content');
+
+                for (var i = 0, len = docs.length; i < len; i++) {
+                    //docs[i]
+                    swig.renderFile(__base + 'server/views/guest_post.html', {
+                            name: docs[i]['FIRST NAME']
+                        },
+                        function(err, content) {
+                            if (err) {
+                                throw err;
+                            }
+                            sendEmails(docs[i]['EMAIL ADD'], subject, content);
+
+                        });
+                };
+
+                reply(docs);
+
+            });
+
+
+
+
+        },
+        app: {
+            name: 'guestEmail'
+        }
     }
-  }
 
 };
