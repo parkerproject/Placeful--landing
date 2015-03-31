08d38b5b75e67e390acacb9066c818b1fcdbc5c6
Parker <parkerproject@gmail.com>
2015-03-31 19:59:42 -0400

    updates

--- server/controllers/giveaway.js
+++ server/controllers/giveaway.js
@@ -19,6 +19,35 @@ function getUsers(cb) {
     });
 }
 
+// function isLastDay(dt) {
+//     var test = new Date(dt.getTime());
+//     test.setDate(test.getDate() + 1);
+//     return test.getDate() === 1;
+// }
+
+
+// function resetPoints() {
+//     var dt = new Date();
+//     var status = isLastDay(dt); // checks if last day of month
+//     var className = 'User';
+//     var update = {
+//         data: {
+//             points: 0
+//         }
+//     };
+
+//     if (status) {
+//         kaiseki.updateObjects(className, update, function(err, res, body, success) {
+//             console.log('status1');
+//             for (var i = 0; i < body.length; i++) {
+//                 object = body[i];
+//                 console.log('objects updated = at ', object.updatedAt);
+//             }
+//         });
+//     }
+
+// }
+
 
 // var job = new CronJob({
 //     cronTime: '0-59', //'00 30 11 * * 1-7',
@@ -45,7 +74,6 @@ module.exports = {
             getUsers(function(body) {
                 var users = _.shuffle(body);
                 var fiveUsers = _.sample(users, 5);
-
 
                 reply(fiveUsers);
             });
