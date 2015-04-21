90e6aacf56e426363608edbe5860688c359b2bd4
parkerproject <parkerproject@gmail.com>
2015-04-21 17:46:40 +0000

    updated the text copy

--- assets.js
+++ assets.js
@@ -2,7 +2,7 @@
 module.exports = {
     development: {
         js: ['js/jquery.min.js', 'js/sweet-alert.js', 'js/tabulous.min.js', 'js/scripts.js'],
-        css: ['css/animate.css', 'css/normalize.css', 'css/skeleton.css', 'css/sweet-alert.css', 'css/tabulous.css', 'css/custom.css']
+        css: ['css/fonts.css','css/animate.css', 'css/normalize.css', 'css/skeleton.css', 'css/sweet-alert.css', 'css/tabulous.css', 'css/custom.css']
     },
     production: {
         js: ['js/scripts.min.js'],
--- server/controllers/base.js
+++ server/controllers/base.js
@@ -4,11 +4,11 @@ var collections = ['deals'];
 var db = require("mongojs").connect(process.env.DEALSBOX_MONGODB_URL, collections);
 var swig = require('swig');
 var _ = require('underscore');
-var raccoon = require('raccoon');
-raccoon.config.nearestNeighbors = 10; // number of neighbors you want to compare a user against
-raccoon.config.className = 'deal'; // prefix for your items (used for redis)
-raccoon.config.numOfRecsStore = 30; // number of recommendations to store per user
-raccoon.connect(process.env.REDIS_PORT, process.env.REDIS_URL, process.env.REDIS_AUTH);
+//var raccoon = require('raccoon');
+//raccoon.config.nearestNeighbors = 10; // number of neighbors you want to compare a user against
+//raccoon.config.className = 'deal'; // prefix for your items (used for redis)
+//raccoon.config.numOfRecsStore = 30; // number of recommendations to store per user
+//raccoon.connect(process.env.REDIS_PORT, process.env.REDIS_URL, process.env.REDIS_AUTH);
 var numeral = require('numeral');
 var Kaiseki = require('kaiseki');
 var kaiseki = new Kaiseki(process.env.PARSE_APP_ID, process.env.PARSE_REST_API_KEY);
@@ -44,14 +44,14 @@ module.exports = {
                     var flatSaved = _.flatten(saved_deals);
                     var uniqueSaved = _.uniq(flatSaved);
 
-                    raccoon.mostLiked(function(results) {
+                  //  raccoon.mostLiked(function(results) {
                         reply.view('index', {
                             title: 'Local deals discovery app - DEALSBOX',
-                            liked: results.length,
+                           // liked: results.length,
                             deals_count: numeral(count).format('0,0'),
                             saved_deals: uniqueSaved.length
                         });
-                    });
+                  //  });
 
                 });
             });
