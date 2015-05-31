fd445250cdf72bb95695608775262f6f1a7aa1b3
Parker <parkerproject@gmail.com>
2015-05-31 17:21:19 -0400

    updates

--- starter.sh
+++ starter.sh
@@ -4,5 +4,5 @@ if [ $(ps aux | grep $USER | grep node | grep -v grep | wc -l | tr -s "\n") -eq 
 then
   export PATH=/usr/local/bin:$PATH
   #export NODE_ENV=production
-  cd /var/www/Dealsbox-landing && forever --spinSleepTime 10000 start server.js >> forever.log 2>&1
+  cd /var/www/Dealsbox-landing && pm2 start server.js >> forever.log 2>&1
 fi
