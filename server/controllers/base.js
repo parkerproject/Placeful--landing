7a26dbf21e16d5687aadabf3599fc9d3139ce05f
Parker <parkerproject@gmail.com>
2015-06-10 22:37:33 -0400

    updates

--- server/controllers/base.js
+++ server/controllers/base.js
@@ -7,7 +7,7 @@ module.exports = {
         handler: function(request, reply) {
 
             reply.view('index', {
-                title: 'Local deals discovery app - DEALSBOX'
+                title: 'Discover and save on local deals - DEALSBOX'
             });
 
         },
