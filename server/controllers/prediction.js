7801329a51f35213a6f2b32fe4a82f1f6c0d3d9f
Parker <parkerproject@gmail.com>
2015-08-21 13:23:02 -0400

    updates

--- server/controllers/prediction.js
+++ server/controllers/prediction.js
@@ -2,14 +2,14 @@ require('dotenv').load();
 var collections = ['keen_events'];
 var db_dev = require("mongojs").connect(process.env.DEV_MONGODB_URL, collections);
 var raccoon = require('raccoon');
-raccoon.config.nearestNeighbors = 10; // number of neighbors you want to compare a user against
+raccoon.config.nearestNeighbors = 5; // number of neighbors you want to compare a user against
 raccoon.config.className = 'DEALSBOX'; // prefix for your items (used for redis)
 raccoon.config.numOfRecsStore = 10; // number of recommendations to store per user
 raccoon.connect(process.env.REDIS_PORT, process.env.REDIS_URL, process.env.REDIS_AUTH);
 var Kaiseki = require('kaiseki');
 var kaiseki = new Kaiseki(process.env.PARSE_APP_ID, process.env.PARSE_REST_API_KEY);
 var Promise = require('es6-promise').Promise;
-var _ = require('lodash')
+var _ = require('lodash');
 
 
 module.exports = {
@@ -20,11 +20,16 @@ module.exports = {
       var action = request.query.action;
       var s = new Date();
       var timestamp = s.toISOString();
-      var dataObj = {event:action, user_id: userId, deal_id: dealId, timestamp: timestamp};
+      var dataObj = {
+        event: action,
+        user_id: userId,
+        deal_id: dealId,
+        timestamp: timestamp
+      };
 
-      db_dev.keen_events.find(dataObj).limit(1, function(err, docs) {
+      db_dev.keen_events.find(dataObj).limit(1, function (err, docs) {
 
-        if(docs.length === 0){
+        if (docs.length === 0) {
           db_dev.keen_events.save(dataObj);
         }
 
--- server/controllers/static.js
+++ server/controllers/static.js
@@ -1,51 +1,61 @@
 // This is the base controller. Used for base routes, such as the default index/root path, 404 error pages, and others.
 module.exports = {
-    index: {
-        handler: function(request, reply) {
-            // Render the view with the custom greeting
-            reply.view('how-it-works');
-        },
-        app: {
-            name: 'index'
-        }
+  index: {
+    handler: function (request, reply) {
+      // Render the view with the custom greeting
+      reply.view('how-it-works');
     },
-
-    terms: {
-        handler: function(request, reply) {
-            // Render the view with the custom greeting
-            reply.view('terms-conditions');
-        },
-        app: {
-            name: 'terms'
-        }
-    },
-    privacy: {
-        handler: function(request, reply) {
-            // Render the view with the custom greeting
-            reply.view('privacy-policy');
-        },
-        app: {
-            name: 'privacy'
-        }
-    },
-    points: {
-        handler: function(request, reply) {
-            // Render the view with the custom greeting
-            reply.view('how-to-earn-points');
-        },
-        app: {
-            name: 'points'
-        },
-
-    },
-    about: {
-        handler: function(request, reply) {
-            // Render the view with the custom greeting
-            reply.view('about');
-        },
-        app: {
-            name: 'about'
-        },
-
+    app: {
+      name: 'index'
     }
+  },
+
+  terms: {
+    handler: function (request, reply) {
+      // Render the view with the custom greeting
+      reply.view('terms-conditions');
+    },
+    app: {
+      name: 'terms'
+    }
+  },
+  privacy: {
+    handler: function (request, reply) {
+      // Render the view with the custom greeting
+      reply.view('privacy-policy');
+    },
+    app: {
+      name: 'privacy'
+    }
+  },
+  points: {
+    handler: function (request, reply) {
+      // Render the view with the custom greeting
+      reply.view('how-to-earn-points');
+    },
+    app: {
+      name: 'points'
+    },
+
+  },
+  about: {
+    handler: function (request, reply) {
+      // Render the view with the custom greeting
+      reply.view('about');
+    },
+    app: {
+      name: 'about'
+    },
+
+  },
+  alerts: {
+    handler: function (request, reply) {
+      // Render the view with the custom greeting
+      reply.view('alerts');
+    },
+    app: {
+      name: 'alerts'
+    },
+
+  }
 };
