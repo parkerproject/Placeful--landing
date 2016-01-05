2ec76278731437481c2662962cddca85c897c7f1
Parker <parkerproject@gmail.com>
2016-01-05 15:39:43 -0500

    fixed facebook tag

--- server/controllers/deal.js
+++ server/controllers/deal.js
@@ -14,6 +14,8 @@ module.exports = {
           var button_text = (deal.provider_name === 'DEALSBOX') ? 'View coupon in app' : 'BUY FROM ' + deal.provider_name;
           var button_url = (deal.provider_name === 'DEALSBOX') ? 'https://app.appsflyer.com/id944253010?pid=deal_page' : deal.url;
           var price = (deal.provider_name === 'DEALSBOX') ? deal.offer : '$' + deal.new_price;
+          var facebook_image_url = "http://imagify.co/img?url=" + deal.large_image + "&crop_width=200&crop_height=200";
+          var facebook_url = 'https://dealsbox.co/deal/' + request.params.deal_id
           req("https://dealsbox.co/lab/yelp_phone?phone=" + JSON.parse(body).phone, function (err, res, data) {
             if (!err && res.statusCode == 200) {
               deal.yelp = JSON.parse(data);
