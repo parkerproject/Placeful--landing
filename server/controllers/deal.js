595559165cfe701534c6eecec6cdb2fcac6935e7
Parker <parkerproject@gmail.com>
2016-01-08 14:18:19 -0500

    updates

--- server/controllers/deal.js
+++ server/controllers/deal.js
@@ -16,7 +16,7 @@ module.exports = {
           var price = (deal.provider_name === 'DEALSBOX') ? deal.offer : '$' + deal.new_price;
           var facebook_image_url = "http://imagify.co/img?url=" + deal.large_image + "&crop_width=200&crop_height=200";
           var facebook_url = 'https://dealsbox.co/deal/' + request.params.deal_id
-          req("https://dealsbox.co/lab/yelp_phone?phone=" + JSON.parse(body).phone, function (err, res, data) {
+          req("https://merchant.dealsbox.co/lab/yelp_phone?phone=" + JSON.parse(body).phone, function (err, res, data) {
             if (!err && res.statusCode == 200) {
               deal.yelp = JSON.parse(data);
               reply.view("deal", {
@@ -30,7 +30,6 @@ module.exports = {
 
             }
           });
-
 
 
 
