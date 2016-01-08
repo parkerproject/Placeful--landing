e48fbc0cfafdbdf74c878fc6f0a9d706b90e857e
Parker <parkerproject@gmail.com>
2016-01-08 14:29:16 -0500

    update

--- server/controllers/deal.js
+++ server/controllers/deal.js
@@ -12,7 +12,7 @@ module.exports = {
         if (!error && response.statusCode == 200) {
           var deal = JSON.parse(body);
           var button_text = (deal.provider_name === 'DEALSBOX') ? 'View coupon in app' : 'BUY FROM ' + deal.provider_name;
-          var button_url = (deal.provider_name === 'DEALSBOX') ? 'https://app.appsflyer.com/id944253010?pid=deal_page' : deal.url;
+          var button_url = (deal.provider_name === 'DEALSBOX') ? 'dealsbox://deal/' + request.params.deal_id : deal.url;
           var price = (deal.provider_name === 'DEALSBOX') ? deal.offer : '$' + deal.new_price;
           var facebook_image_url = "http://imagify.co/img?url=" + deal.large_image + "&crop_width=200&crop_height=200";
           var facebook_url = 'https://dealsbox.co/deal/' + request.params.deal_id
