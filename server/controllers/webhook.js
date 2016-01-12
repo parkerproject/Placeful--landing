726817f09a858fc2834f0f94536fb29c22a514be
Parker <parkerproject@gmail.com>
2016-01-11 19:33:18 -0500

    testing webhook

--- server/controllers/webhook.js
+++ server/controllers/webhook.js
@@ -1,5 +1,5 @@
 require('dotenv').load();
-var collections = ['webhook'];
+var collections = ['webhook', 'deals'];
 var db = require("mongojs").connect(process.env.DEALSBOX_MONGODB_URL, collections);
 // var intercom = require('intercom.io');
 var Keen = require("keen-js");
@@ -15,22 +15,44 @@ module.exports = {
     handler: function (request, reply) {
 
       var data = request.payload.data.item.metadata;
+      db.webhook.save(request.payload);
 
-      if (data) {
-        data.keen = {
-          timestamp: new Date().toISOString()
-        };
-      }
+      // if (data) {
+      //   data.keen = {
+      //     timestamp: new Date().toISOString()
+      //   };
+      // }
 
-      db.webhook.save(data);
+      // db.deals.find({
+      //   deal_id: data.deal_id
+      // }).limit(1, function (err, result) {
+      //   db.webhook.findAndModify({
+      //     query: {
+      //       deal_id: result.deal_id
+      //     },
+      //     update: {
+      //       $inc: {
+      //         view_count: 1
+      //       }
+      //     },
+      //     new: true,
+      //     upsert: true
+      //   }, function (err, doc, lastErrorObject) {
+      //     // doc.tag === 'maintainer'
+      //     client.addEvent("views", data, function (err, res) {
+      //       if (err) {
+      //         console.log(err);
+      //       } else {
+      //         console.log(res);
+      //       }
+      //     });
+      //   });
+      //
+      // });
 
-      client.addEvent("views", data, function (err, res) {
-        if (err) {
-          console.log(err);
-        } else {
-          console.log(res);
-        }
-      });
+      //db.webhook.save(data);
+
+
     }
   }
 };
