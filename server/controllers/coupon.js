a547d6ae80799d848b9b6afb8f04956e62e905f5
Parker <parkerproject@gmail.com>
2016-04-03 21:34:51 -0400

    updated strcit mode

--- server/controllers/coupon.js
+++ server/controllers/coupon.js
@@ -1,14 +1,12 @@
-require('dotenv').load();
-var swig = require('swig');
-var collections = ['merchants'];
-var db = require("mongojs").connect(process.env.DEALSBOX_MONGODB_URL, collections);
-var _request = require('request');
-var Kaiseki = require('kaiseki');
-var kaiseki = new Kaiseki(process.env.PARSE_APP_ID, process.env.PARSE_REST_API_KEY);
-var Joi = require('joi');
-
-
-
+'use strict'
+require('dotenv').load()
+var swig = require('swig')
+var collections = ['merchants']
+var db = require('mongojs').connect(process.env.DEALSBOX_MONGODB_URL, collections)
+var _request = require('request')
+var Kaiseki = require('kaiseki')
+var kaiseki = new Kaiseki(process.env.PARSE_APP_ID, process.env.PARSE_REST_API_KEY)
+var Joi = require('joi')
 module.exports = {
   index: {
     handler: function (request, reply) {
@@ -16,18 +14,16 @@ module.exports = {
         where: {
           email: request.query.email
         }
-      };
-
+      }
       kaiseki.getUsers(params, function (err, res, body, success) {
         if (!body.hasOwnProperty('error')) {
           reply.view('referral_coupon', {
             code: body[0].objectId
-          });
+          })
         } else {
-          reply('<h2>Invalid referral link</h2>');
+          reply('<h2>Invalid referral link</h2>')
         }
-
-      });
+      })
     },
     validate: {
       query: {
@@ -35,5 +31,4 @@ module.exports = {
       }
     }
   }
-
-};
+}
